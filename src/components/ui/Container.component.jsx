e42ed078ff301a5461a64fecaf2b87b0496426d2
Mohan Sharma <mohansharma201.ms@gmail.com>
2021-07-29 22:06:33 +0530

    added supabase

--- src/components/ui/Container.component.jsx
+++ src/components/ui/Container.component.jsx
@@ -1,7 +1,7 @@
 const Container = ({ children }) => {
   return (
-    <section class="py-5">
-      <div class="container px-4 px-lg-5 mt-5">{children}</div>
+    <section className="py-5">
+      <div className="container px-4 px-lg-5 mt-5">{children}</div>
     </section>
   );
 };
